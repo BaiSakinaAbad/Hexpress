e8a6a5dee38f415a2cac92e635aac4c382bf2482
Saira Sofia De Mesa <148078310+sairarat@users.noreply.github.com>
2025-03-13 14:58:36 +0800

    Updated Product.java

--- Product.java
+++ Product.java
@@ -4,24 +4,30 @@ public class Product {
 	String product_Name;
 	int product_Weight;
 	int product_Value;
-	
+
 	public Product(String product_Name, int product_Weight, int product_Value) {
 		this.product_Name = product_Name;
 		this.product_Weight = product_Weight;
 		this.product_Value = product_Value;
 	}
-	
-	Product[] product = {
-			new Product("Apple", 3, 350),
-			new Product("flower vase", 5, 555),
-	        new Product("big stuff toy", 7, 711),
-	        new Product("cat food", 6, 600),
-	        new Product("books", 4, 469)
-	};
-	
-	//for printing weight with kg
-	//just add "kg" using loop
-	//or recursion if possible
-	
-}
 
+	static Product[] product = {
+			new Product("Apple", 3, 350),
+			new Product("Flower Vase", 5, 555),
+			new Product("Big Stuff Toy", 7, 711),
+			new Product("Arrows", 6, 600),
+			new Product("Books", 4, 469)
+	};
+
+	@Override
+	public String toString() {
+		return product_Name + " - " + product_Weight + "kg - " + product_Value;
+	}
+
+	public static void printProductList() {
+		System.out.println("Product List:");
+		for (Product p : product) {
+			System.out.println(p);
+		}
+	}
+}
