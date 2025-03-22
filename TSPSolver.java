032618a5f3f9f393844ade7adb610ca6eb5c9f71
Bai Sakina Abad <kimireneabad07@gmail.com>
2025-03-22 17:14:26 +0800

    Update TSPSolver.java

--- TSPSolver.java
+++ TSPSolver.java
@@ -1,17 +1,16 @@
-
 import java.util.*;
 
 public class TSPSolver {
     private static final int[][] distanceMatrix = {
-            {0, 15, 14, 9, 10},
-            {15, 0, 4, 4, 20},
-            {14, 4, 0, 5, 17},
-            {9, 4, 5, 0, 15},
-            {10, 20, 17, 15, 0}
+            {0, 15, 14, 9, 10}, // Koriko City
+            {15, 0, 4, 4, 20},  // Emishi Village
+            {14, 4, 0, 5, 17},  // Kingsbury
+            {9, 4, 5, 0, 15},   // Howl's Castle
+            {10, 20, 17, 15, 0} // Hotel Adriano
     };
     static final String[] cities = {"Koriko City", "Emishi Village", "Kingsbury", "Howl's Castle", "Hotel Adriano"};
 
-    public static void runTSP(List<String> batchLocations) {
+    public static int runTSP(List<String> batchLocations) {
         List<Integer> locationIndices = new ArrayList<>();
         for (String loc : batchLocations) {
             for (int i = 0; i < cities.length; i++) {
@@ -23,13 +22,13 @@ public class TSPSolver {
 
         if (locationIndices.isEmpty()) {
             System.out.println("No valid locations for TSP.");
-            return;
+            return 0; // Return 0 if no valid locations
         }
 
-        solveTSP(locationIndices);
+        return solveTSP(locationIndices);
     }
 
-    private static void solveTSP(List<Integer> locations) {
+    private static int solveTSP(List<Integer> locations) {
         int minDistance = Integer.MAX_VALUE;
         List<Integer> bestRoute = null;
 
@@ -47,7 +46,9 @@ public class TSPSolver {
         if (bestRoute != null) {
             System.out.println("\nShortest Route: Koriko City -> " + bestRoute.stream().map(i -> cities[i]).toList() + " -> Koriko City");
             System.out.println("Distance: " + minDistance + " km\n");
+            return minDistance; // Return the minimum distance
         }
+        return 0; // Return 0 if no route is found
     }
 
     private static void permute(List<Integer> arr, int len, List<List<Integer>> results) {
